b41f5f765420355d48aae5d5d0aded9a225c37ad
PINTU <PINTU@DESKTOP-MQFG0DI>
2019-11-22 00:07:48 +0530

    initial commit

---
+++ src/main/java/com/iodia/eduodia/EduodiaApplication.java
@@ -0,0 +1,13 @@
+package com.iodia.eduodia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduodiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduodiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iodia/eduodia/EduodiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iodia.eduodia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduodiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
